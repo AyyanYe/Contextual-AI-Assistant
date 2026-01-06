e213390816b0ec5eccd38b30c90acae379526e92
Ayyan Ahmed <121036267+AyyanYe@users.noreply.github.com>
2026-01-06 04:04:38 +0000

    Full environment snapshot with Context AI app - 2024-06

---
+++ nextcloud/config/config.php
@@ -0,0 +1,18 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocodtucztg5x',
+  'passwordsalt' => 'c3T2KuB5uTHtY9NRB9qjgdieev8HeN',
+  'secret' => 'PSUWj9vEDEaykDkdbSrMVvEFL6hT4tb1xYNA+iYeEExWW6Cl',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost:8080',
+  ),
+  'datadirectory' => '/workspaces/Contextual-AI-Assistant/nextcloud/data',
+  'dbtype' => 'sqlite3',
+  'version' => '32.0.3.2',
+  'overwrite.cli.url' => 'https://cautious-space-guacamole-r96p9q974vphx44r-8080.app.github.dev',
+  'installed' => true,
+  'maintenance' => false,
+  'debug' => true,
+  'overwriteprotocol' => 'https',
+);
